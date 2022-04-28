b7efeb29b4dc0c005c226dbd227dc9d2b0d02666
Drvondevious <william.prometheus@gmail.com>
2022-04-27 21:05:33 -0500

    generate tree with seed file

--- api/prisma/seed.ts
+++ api/prisma/seed.ts
@@ -1,5 +1,24 @@
-// import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Tree } from '@prisma/client';
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
-// const main = async () => {};
+const main = async () => {
+  console.log('seeding database...');
+
+  console.log('generating trees...');
+  const tree01: Tree = await prisma.tree.create({
+    data: {
+      ownerId: 'auth0|626431bbfad45200691f15bc',
+      name: 'Web Development',
+      description: 'Learn to build a website from this in-depth Skilltree!',
+    },
+  });
+  console.log(`created tree with id ${tree01.id}`);
+};
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+}).finally(async () => {
+  await prisma.$disconnect();
+});
